aef34343d1448745fb5896e9bc4701e333d85f03
cutefcc <cutefcc@126.com>
2022-08-28 22:11:07 +0800

    test connect wallet

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -8,10 +8,13 @@ contract MetaCoin is ERC20 {
     string public constant SYMBOL = "CHECHE"; // cheche token
     uint256 public constant INITIAL_SUPPLY = 10000; // 10,000 cheche tokens
 
+    // 只有在部署时执行一次
     constructor() ERC20(NAME, SYMBOL) {
+        // initialize the total supply of cheche tokens
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
+    // 重写 decimals
     function decimals() public view virtual override returns (uint8) {
         return 0;
     }
--- web/index2.js
+++ web/index2.js
@@ -1,13 +1,16 @@
-// const { ethers } = require("ethers");
-
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 let signer = "";
-$(document).on("click", "#wallet", async () => {
+document.getElementById("wallet").addEventListener("click", async () => {
+  // 链接钱包
   await provider.send("eth_requestAccounts", []);
+  // 获取签名
   signer = provider.getSigner();
+  // 获取钱包地址
   const address = await signer.getAddress();
+  console.log("address", address);
   $("#address").html(address);
+
   let pendingBal = await provider.getBalance(address, "padding");
   const balance = ethers.utils.formatEther(pendingBal);
   console.log("balance", balance);
